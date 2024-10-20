5d80a889eb0753fe6401b0fa72869682b43ddc63
Jeff Brown <jeff@jeffbrown.tech>
2024-10-20 14:08:40 -0700

    pipeline updates

--- 2024-09-30-PowerShellAdvancedFunctionExample/AdvancedFunctionExample.ps1
+++ 2024-09-30-PowerShellAdvancedFunctionExample/AdvancedFunctionExample.ps1
@@ -15,7 +15,7 @@ $logicApp.Definition.ToString() | Out-File -FilePath .\mylogicappbackup.json
 function Export-LogicAppDefinition {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)] # ValueFromPipeline and ValueFromPipelineByPropertyName allow processing input objects through the pipeline
         [string]
         $Name,
 
@@ -30,23 +30,54 @@ function Export-LogicAppDefinition {
         $FilePath = (Get-Location),
 
         [Parameter()]
-        [ValidatePattern("\.json$", ErrorMessage="'{0}' should have a .json file extension.")]
+        [ValidatePattern("\.json$", ErrorMessage = "'{0}' should have a .json file extension.")]
         [string]
-        $FileName = "$($Name)_$(Get-Date -Format FileDateTimeUniversal).json"
+        $FileName
     )
 
-    $logicApp = Get-AzLogicApp -Name $Name
+    # Process blocks automatically loop through any input objects coming through the pipeline
+    process {
+        $logicApp = Get-AzLogicApp -Name $Name
 
-    if ($logicApp) {
-        try {
-            $logicApp.Definition.ToString() | Out-File -FilePath "$FilePath\$FileName" -ErrorAction STOP
+        # Creating process-specific variable so it can be nulled out when executing across multiple pipeline inputs
+        $processFileName = $FileName
+
+        # If the LogicApp exists
+        if ($logicApp) {
+            # Using Write methods to output to console
+            Write-Verbose -Message "Processing $Name"
+
+            # Creating a process-loop specific variable
+            if (-not $processFileName) {
+                $processFileName = "$($Name)_$(Get-Date -Format FileDateTimeUniversal).json"
+            }
+
+            # try/catch/finally block for error handling
+            try {
+                Write-Verbose -Message "Exporting Logic App definition to $processFileName."
+                $logicApp.Definition.ToString() | Out-File -FilePath "$FilePath\$processFileName" -ErrorAction STOP
+                $file = Get-Item -Path "$FilePath\$processFileName"
+
+                # Creating a custom object to output what the function did
+                $output = [PSCustomObject]@{
+                    LogicApp = $logicApp.Name
+                    File     = $file.FullName
+                }
+
+                $output
+            }
+            catch {
+                $errorMessage = (Get-Error -Newest 1).Exception.Message
+                Write-Warning -Message "There was an issue exporting the Logic App definition for $Name : $errorMessage"
+            }
+            finally {
+                # Finally blocks always execute whether or not an error occurred. In this case, nulling out a variable.
+                $processFileName = $null
+            }
         }
-        catch {
-            $errorMessage = (Get-Error -Newest 1).Exception.Message
-            Write-Warning -Message "There was an issue exporting the Logic App definition for $Name : $errorMessage"
-        }        
-    }
-    else {
-        Write-Warning -Message "No Logic Apps found named $Name. Double check your spelling or current subscription context using Get-AzContext."
-    }
+        # If the LogicApp does not exist
+        else {
+            Write-Warning -Message "No Logic Apps found named $Name. Double check your spelling or current subscription context using Get-AzContext."
+        }
+    } # End process lbock
 }
